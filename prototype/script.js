4133b049dd3eafac447be5feee96f904160518aa
mohamedyahya <mohamed.yahya.dev@gmail.com>
2021-12-21 12:45:38 +0100

    Un changement

--- prototype/script.js
+++ prototype/script.js
@@ -22,7 +22,7 @@ function Calculate(A,B,operation){
 
 
 // Presentation
-function ClickNumber(number){
+function CliquezNuméro(number){
     if(A == undefined){
         if(AString == undefined) AString = '';
         AString += number;
