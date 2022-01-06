c1dfe003bfe3c652452a9ea2f72ad302e0cfa2e8
mohamedyahya <mohamed.yahya.dev@gmail.com>
2022-01-06 12:42:42 +0100

    up

--- prototype2/calculatrice-oop.js
+++ prototype2/calculatrice-oop.js
@@ -9,51 +9,51 @@ class CalculatriceBLO {
 
     // Setters and getters
     get a() {
-      return this._a
+      return this.#_a
     }
     set a(value){
-        this._a = value;
+        this.#_a = value;
     }
     get b() {
-        return this._b
+        return this.#_b
     }
     set b(value){
-          this._b = value;
+          this.#_b = value;
     }
  
     get operation() {
-        return this._operation
+        return this.#_operation
     }
     set operation(value){
-          this._operation = value;
+          this.#_operation = value;
     }
 
     get solution() {
-        return this._solution
+        return this.#_solution
     }
     set solution(value){
-          this._solution = value;
+          this.#_solution = value;
     }
 
     Calculer(){
-        this._solution = undefined;
-        switch (this._operation) {
+        this.#_solution = undefined;
+        switch (this.#_operation) {
             case '+':
-                this._solution = this._a + this._b;
+                this.#_solution = this.#_a + this.#_b;
                 break;
             case '-':
-                this. _solution = this._a - this._b;
+                this. #_solution = this.#_a - this.#_b;
                     break;
             default:
                 break;
         }
-        return this._solution;
+        return this.#_solution;
     }
 
     Init(){
-        this._a = undefined;
-        this._b = undefined;
-        this._operation = undefined;
+        this.#_a = undefined;
+        this.#_b = undefined;
+        this.#_operation = undefined;
     }
 
   }
